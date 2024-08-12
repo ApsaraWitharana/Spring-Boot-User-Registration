6190fe50bc263972378b5e9a591f49f7fb3d0070
ApsaraWitharana <asachini095@gmail.com>
2024-08-12 16:16:11 +0530

    update-done

--- src/main/java/lk/ijse/gdse68/user_registation/controller/UserController.java
+++ src/main/java/lk/ijse/gdse68/user_registation/controller/UserController.java
@@ -13,14 +13,23 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    //save//
     @PostMapping("/savepost")
     public void savePost(@RequestBody User user){
        userRepository.save(user);
     }
 
+    //update//
     @PutMapping("/updatepost")
     public User updatePost(@RequestBody User user){
         return userRepository.save(user);
     }
+
+    //delete//
+    @DeleteMapping("/deletepost/{username}")
+    public void deletePost(@PathVariable String username){
+        userRepository.deleteById(username);
+    }
+
 
 }
